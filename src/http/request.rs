1679707dda942bb1ae0e57d63b44366f8f1a8712
Leonardo Moraes <leonardobastosmoraes@hotmail.com>
2024-05-14 17:17:11 -0300

    Third commit

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,9 +1,73 @@
 
+use std::str::Utf8Error;
+use std::str;
 use super::method::Method;
-
+use std::convert::TryFrom;
+use std::error::Error;
+use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
 
 pub struct Request {
     path: String,
     query_string: Option<String>,
     method: Method,
 }
+
+impl TryFrom<&[u8]> for Request {
+    type Error = ParseError;
+
+
+    //Get /search?name=abc&sort=1 HTTP/1.1
+    fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+        let request = str::from_utf8(buf)?;
+        unimplemented!()
+    }
+} 
+
+fn get_next_word(request: &str) -> Option<(&str, &str)> {
+    for (i,c) in request.chars().enumerate() {
+        if c == ' ' {
+            return Some((&request[..i], &request[i + 1..]));
+        }
+    }
+    None
+}
+
+
+pub enum ParseError {
+    InvalidRequest,
+    InvalidEncoding,
+    Invalidprotocol,
+    InvalidMethod,
+}
+
+impl ParseError {
+    fn message(&self) -> &str {
+        match self {
+                Self::InvalidRequest => "Invalid REquest",
+                Self::InvalidEncoding => "Invalid Encoding",
+                Self::Invalidprotocol => "Invalid Protocol",
+                Self::InvalidMethod => "Invalid Method", 
+        }
+    }
+}
+
+impl From<Utf8Error> for ParseError {
+    fn from(_: Utf8Error) -> Self {
+        Self::InvalidEncoding
+    }
+}
+
+impl Display for ParseError {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl Debug for ParseError {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl Error for ParseError {}
+
